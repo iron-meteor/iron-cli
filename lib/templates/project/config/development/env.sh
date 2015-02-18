7d31b8dfda514eade9a2c6cd2077450bd64521e7
Chris Mather <mather.chris@gmail.com>
2015-02-18 13:25:58 -0800

    fix 'exports' to be 'export' in sh file

--- lib/templates/project/config/development/env.sh
+++ lib/templates/project/config/development/env.sh
@@ -3,7 +3,7 @@
 # to the server side of your app by using process.env.MY_VAR
 #
 # Example:
-# exports MONGO_URL="mongodb://localhost:27017/myapp-development"
-# exports ROOT_URL="http://localhost:3000"
+# export MONGO_URL="mongodb://localhost:27017/myapp-development"
+# export ROOT_URL="http://localhost:3000"
 
-exports SAMPLE_VARIABLE="somevalue"
+export SAMPLE_VARIABLE="somevalue"
