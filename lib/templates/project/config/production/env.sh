28d1b1e3f3f63dc212cea0cc5fbcddfa0a9471c0
Chris Butler <chris@platformthirteen.com>
2015-07-18 05:06:24 -0400

    generate production folder by default

---
+++ lib/templates/project/config/production/env.sh
@@ -0,0 +1,9 @@
+# This .sh file will be sourced before starting your application.
+# You can use it to put environment variables you want accessible
+# to the server side of your app by using process.env.MY_VAR
+#
+# Example:
+# export MONGO_URL="mongodb://localhost:27017/myapp-development"
+# export ROOT_URL="http://localhost:3000"
+
+export SAMPLE_VARIABLE="somevalue"
